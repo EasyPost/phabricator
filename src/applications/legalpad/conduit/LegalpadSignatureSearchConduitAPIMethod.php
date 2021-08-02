ba27cd7b594e943e39b0d7d554e21e64ba742862
James Brown <jbrown@easypost.com>
2021-08-02 15:38:40 -0700

    improve LegalpadDocumentSignatureSearchEngine; fix bug in LegalpadDocumentSearchEngine; add conduit methods

---
+++ src/applications/legalpad/conduit/LegalpadSignatureSearchConduitAPIMethod.php
@@ -0,0 +1,19 @@
+<?php
+
+final class LegalpadSignatureSearchConduitAPIMethod
+  extends PhabricatorSearchEngineAPIMethod {
+
+  public function getAPIMethodName() {
+    return 'legalpad.signature.search';
+  }
+
+  public function newSearchEngine() {
+    return new LegalpadDocumentSignatureSearchEngine();
+  }
+
+  public function getMethodSummary() {
+    return pht('Read information about legalpad document signatures.');
+  }
+
+}
+?>
---
+++ src/applications/legalpad/engineextension/PhabricatorLegalpadSignaturesSearchEngineAttachment.php
@@ -0,0 +1,36 @@
+<?php
+
+final class PhabricatorLegalpadSignaturesSearchEngineAttachment
+  extends PhabricatorSearchEngineAttachment {
+
+  public function getAttachmentName() {
+    return pht('Document signers');
+  }
+
+  public function getAttachmentDescription() {
+    return pht('Get the signer list for the project.');
+  }
+
+  public function willLoadAttachmentData($query, $spec) {
+    $query->needSignatures(true);
+  }
+
+  public function getAttachmentForObject($object, $data, $spec) {
+    $signers = array();
+    foreach ($object->getSignatures() as $signature) {
+      $signatures[] = array(
+        'phid' => $signature->getPHID(),
+        'signerPHID' => $signature->getSignerPHID(),
+        'exemptionPHID' => $signature->getExemptionPHID(),
+        'isExemption' => $signature->getIsExemption(),
+        'signerName' => $signature->getSignerName(),
+        'signerEmail' => $signature->getSignerEmail(),
+      );
+    }
+
+    return array(
+      'signatures' => $signatures,
+    );
+  }
+
+}
